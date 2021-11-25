b89aee143d482b53ccdda240d90adaa0249bfa12
Kishan Salgado <ksalgado@ksalgado-mn2.linkedin.biz>
2021-11-25 11:24:25 -0500

    Added new files for windows and linux

---
+++ Microsoft.Compute/vm-simple-linux/deploy.ps1
@@ -0,0 +1,13 @@
+$projectName = Read-Host -Prompt "Enter a project name with 12 or less letters or numbers that is used to generate Azure resource names"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$adminUserName = Read-Host -Prompt "Enter the virtual machine administrator account name"
+$adminPassword = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
+$dnsLabelPrefix = Read-Host -Prompt "Enter the DNS label prefix (i.e. myapp)"
+
+$resourceGroupName = "${projectName}-rg"
+$templateUri = "https://raw.githubusercontent.com/kishsal/Arm-quickstart/main/Microsoft.Compute/vm-simple-linux/azuredeploy.json"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -projectName $projectName -location $location -dnsLabelPrefix $dnsLabelPrefix -adminUsername $adminUsername -adminPassword $adminPassword -Verbose
+
+Write-Host "Press [ENTER] to continue."
